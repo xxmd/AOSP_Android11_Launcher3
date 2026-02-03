26b8a3834fd63b7986b71d1c1fabfa7fac0b153d
xxmd <xxmd3720@gmail.com>
2026-02-03 16:05:04 +0800

    Update: mk文件引入launcher_client.jar

--- Android.mk
+++ Android.mk
@@ -131,7 +131,6 @@ LOCAL_USE_AAPT2 := true
 LOCAL_AAPT2_ONLY := true
 LOCAL_MODULE_TAGS := optional
 
-
 LOCAL_STATIC_JAVA_LIBRARIES := \
     SystemUI-statsd \
     SystemUISharedLib \
@@ -139,8 +138,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     launcher_log_protos_lite \
     lib_launcher_client
 
-include $(BUILD_PACKAGE)
-include $(CLEAR_VARS)
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := lib_launcher_client:libs/launcher_client.jar
 include $(BUILD_MULTI_PREBUILT)
 
